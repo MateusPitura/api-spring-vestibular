8ac870c70265b0fa109e0c689cc0343c7022cb36
MateusPitura <mateuspitura@gmail.com>
2024-03-29 11:36:28 -0300

    feat: mais propriedades as tabelas
    Adicionados mais campos e constraints nas tabelas do banco de dados

--- src/main/resources/database/V0__schema.sql
+++ src/main/resources/database/V0__schema.sql
@@ -4,15 +4,16 @@ use vestibulares;
 
 create table if not exists universidades(
     id bigint auto_increment not null,
-    nome varchar(128),
-    sigla varchar(16),
+    nome varchar(128) not null,
+    sigla varchar(16) not null,
+    url_image varchar(256) not null,
     primary key(id)
 );
 
 create table if not exists eventos(
     id bigint auto_increment not null,
-    data timestamp,
-    descricao varchar(255),
+    data timestamp not null,
+    descricao varchar(255) not null,
     id_universidade bigint not null,
     primary key(id),
     foreign key(id_universidade) references universidades(id)
@@ -20,9 +21,10 @@ create table if not exists eventos(
 
 create table if not exists avisos(
     id bigint auto_increment not null,
-    data timestamp,
-    descricao varchar(255),
-    url varchar(255),
+    data date not null,
+    title varchar(128) not null,
+    descricao varchar(256) not null,
+    url varchar(256) not null,
     id_universidade bigint not null,
     primary key(id),
     foreign key(id_universidade) references universidades(id)
