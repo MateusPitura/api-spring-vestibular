e8b1c516ba8e13fcc93b089604fe82e8c7961fa9
MateusPitura <mateuspitura@gmail.com>
2024-04-07 18:56:53 -0300

    feat: delete eventos

--- src/main/java/br/com/mateus/api/eventos/ControllerEventos.java
+++ src/main/java/br/com/mateus/api/eventos/ControllerEventos.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,11 @@ public class ControllerEventos {
             universidade = repositoryUnividades.getReferenceById(dto.universidadeId());
         }
         eventos.update(dto, universidade);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void delete(@PathVariable Long id){
+        repositoryEventos.deleteById(id);
     }
 }
